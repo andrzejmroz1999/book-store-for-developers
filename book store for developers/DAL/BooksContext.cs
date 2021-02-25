f50c290d5239e0f7c56203a6d082771aecddc4bf
Andrzej Mróz <andrzejmroz1999@gmail.com>
2021-02-25 17:03:09 +0100

    adding BooksContext

---
+++ book store for developers/DAL/BooksContext.cs
@@ -0,0 +1,32 @@
+﻿using book_store_for_developers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace book_store_for_developers.DAL
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext() : base("BooksContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
