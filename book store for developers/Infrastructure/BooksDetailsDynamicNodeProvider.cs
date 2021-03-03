5c7e1d6dcc4d1746f69d36afb14d8eaad6fa4964
Andrzej Mróz <andrzejmroz1999@gmail.com>
2021-03-03 16:01:59 +0100

    adding navigation path

---
+++ book store for developers/Infrastructure/BooksDetailsDynamicNodeProvider.cs
@@ -0,0 +1,30 @@
+﻿using book_store_for_developers.DAL;
+using book_store_for_developers.Models;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace book_store_for_developers.Infrastructure
+{
+    public class BooksDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private BooksContext db = new BooksContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach (Book book in db.Books)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = book.BookTitle;
+                node.Key = "Book_" + book.BookId;
+                node.ParentKey = "Category_" + book.CategoryId;
+                node.RouteValues.Add("id", book.BookId);
+                returnValue.Add(node);
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ book store for developers/Infrastructure/CategoriesDetailsDynamicNodeProvider.cs
@@ -0,0 +1,29 @@
+﻿using book_store_for_developers.DAL;
+using book_store_for_developers.Models;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace book_store_for_developers.Infrastructure
+{
+    public class CategoriesDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private BooksContext db = new BooksContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach (Category category in db.Categories)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = category.CategoryName;
+                node.Key = "Category_" + category.CategoryId;            
+                node.RouteValues.Add("categoryName" ,category.CategoryName);
+                returnValue.Add(node);
+            }
+
+            return returnValue;
+        }
+    }
+}
