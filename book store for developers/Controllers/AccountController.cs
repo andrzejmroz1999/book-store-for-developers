e18376f4eb2d06e06713daeeef9101a9678d8d9c
Andrzej Mróz <andrzejmroz1999@gmail.com>
2021-03-06 17:47:12 +0100

    Adding registration and login views

---
+++ book store for developers/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using book_store_for_developers.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace book_store_for_developers.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }          
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index","Home");
+            }
+            
+        }
+    }
+}
---
+++ book store for developers/ViewModels/AccountViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace book_store_for_developers.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Enter e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+         
+        [Required(ErrorMessage = "Enter password")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "remember me")]
+        public bool RememberMe { get; set; }
+
+    }
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The email field is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [StringLength(30,ErrorMessage = "{0} must have at least {2} characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "The password field is required")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",ErrorMessage = "Password and confirm password are different")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
